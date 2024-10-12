d1ca48822d52dc9ffa6a1f02c808788ce1a21776
CharithCD <damruwanweerasinghe1@gmail.com>
2024-10-12 10:08:24 +0530

    Add a empty page for report download page

---
+++ app/download/page.tsx
@@ -0,0 +1,29 @@
+"use client";
+import { Coins } from "lucide-react";
+
+export default function DownloadsPage() {
+  return (
+    <div className="p-8 max-w-4xl mx-auto">
+      <h1 className="text-3xl font-semibold mb-6 text-gray-800">
+        Download Reports
+      </h1>
+
+      <div className="bg-white p-6 rounded-xl shadow-lg flex flex-col justify-between h-full border-l-4 border-green-500 mb-8">
+        <h2 className="text-xl font-semibold mb-4 text-gray-800">
+          Reward Balance
+        </h2>
+        <div className="flex items-center justify-between mt-auto">
+          <div className="flex items-center">
+            <Coins className="w-10 h-10 mr-3 text-green-500" />
+            <div>
+              <span className="text-4xl font-bold text-green-500">
+                blance
+              </span>
+              <p className="text-sm text-gray-500">Available Points</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
